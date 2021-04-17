1c239dff2954e4ebd96dfd1955812e9dc47cb70c
Jennifer Tang-Tran <phuongtangtran@gmail.com>
2021-04-17 14:12:23 -0700

    Updated environment variables

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import './App.css';
 import Recipe from './Components/Recipe';
 
 const App = () => {
-  const APP_ID='3d215a4e';
-  const APP_KEY='8fc9ee21c3d692a895fbc38dfdc647ec';
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState('chicken')
 
   useEffect(() => {
+    console.log(process.env)
     getRecipes();
   }, [query]);
 
   const getRecipes = async () => {
-    const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.React_App_APP_ID}&app_key=${process.env.React_App_APP_KEY}`);
     const data = await res.json();
     setRecipes(data.hits);
   }
